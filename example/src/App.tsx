ce529d4259cec3e43dbe1707cf6e4c52d83feb84
Viktor Stoyanov <vikman.enterprise@gmail.com>
2026-07-10 14:10:14 +0200

    chore(lib): initial commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { multiply } from 'react-native-incar';
 
-const result = multiply(3, 7);
+const result = multiply(5, 10);
 
 export default function App() {
   return (
--- src/multiply.native.tsx
+++ src/multiply.native.tsx
@@ -1,8 +1,7 @@
 import { NitroModules } from 'react-native-nitro-modules';
 import type { Incar } from './Incar.nitro';
 
-const IncarHybridObject =
-  NitroModules.createHybridObject<Incar>('Incar');
+const IncarHybridObject = NitroModules.createHybridObject<Incar>('Incar');
 
 export function multiply(a: number, b: number): number {
   return IncarHybridObject.multiply(a, b);
